2c2da706293c0bd5b2a1cf6f7429c2fa67311c87
OlgaG <osgerasimenko@yandex.ru>
2025-06-29 00:50:04 +0300

    5th task: 04-request-lifecycle Middleware, Pipes, Guards, Interceptors и Error Handlers implemented

--- 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
+++ 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
@@ -2,5 +2,10 @@ import { NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
 export class LoggingMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req;
+
+    console.log(`[${method}] ${originalUrl}`);
+    next();
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
+++ 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
@@ -1,5 +1,13 @@
-import { PipeTransform } from "@nestjs/common";
+import { PipeTransform, Injectable, BadRequestException } from "@nestjs/common";
 
+@Injectable()
 export class ParseIntPipe implements PipeTransform {
-  transform(value: string): number {}
+  transform(value: string): number {
+    const parsedValue = parseInt(value, 10);
+    if (isNaN(parsedValue)) {
+      throw new BadRequestException(`"${value}" не является числом`);
+    }
+
+    return parsedValue;
+  }
 }
