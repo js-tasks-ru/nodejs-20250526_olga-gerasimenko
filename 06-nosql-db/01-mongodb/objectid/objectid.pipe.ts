6a362201b6da2ea1e40df7e6064bd8d78e0b0388
OlgaG <osgerasimenko@yandex.ru>
2025-07-24 18:36:34 +0300

    7th task: 06-nosql-db/01-mongodb implemented,  6th task 05-db/01-crud-tasks review fixed

--- 06-nosql-db/01-mongodb/objectid/objectid.pipe.ts
+++ 06-nosql-db/01-mongodb/objectid/objectid.pipe.ts
@@ -3,5 +3,15 @@ import { isValidObjectId, Types } from "mongoose";
 
 @Injectable()
 export class ObjectIDPipe implements PipeTransform {
-  transform(value: string) {}
+  transform(value: string | Types.ObjectId) {
+    if (value instanceof Types.ObjectId) {
+      return value;
+    }
+
+    if (typeof value === 'string' && isValidObjectId(value)) {
+      return new Types.ObjectId(value);
+    }
+
+    throw new BadRequestException('not a valid object id');
+  }
 }
